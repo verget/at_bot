5855b39ab4d88e27d02d39a98da07d58e5c2dc8f
Andrei Kovalev <verget49@gmail.com>
2019-05-16 18:29:02 +0300

    use async/await instead of promises

--- index.js
+++ index.js
@@ -4,61 +4,34 @@ let url = 'https://author.today/reader/21513/153798';
 const fs = require('fs');
 let text = '';
 
-puppeteer
-  .launch()
-  .then(browser => {
-    return browser.newPage();
-  })
-  .then(page => {
-    const urlArray = url.split('/');
-    let pageNumber = urlArray.slice(-1)[0] * 1;
-    const promises = [];
-    while(pageNumber < 153800) {
-      urlArray[urlArray.length - 1] = ++pageNumber;
-      url = urlArray.join('/');
-      console.log(url);
-      promises.push(parsePage(page, url));
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const urlArray = url.split('/');
+  let pageNumber = urlArray.slice(-1)[0] * 1;
+  for (let i = pageNumber; i < 153800; i++) {
+    console.log(i);
+    urlArray[urlArray.length - 1] = ++pageNumber;
+    url = urlArray.join('/');
+    await parsePage(page, url);
+  }
+  fs.writeFile("./test.fb2", text, function(err) {
+    if (err) {
+      return console.log(err);
     }
-    return promiseSerial(promises);
-  })
-  .then( _ => {
-    fs.writeFile("./test.fb2", text, function(err) {
-      if(err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    });
-  })
-  .catch(function(err) {
-    //handle error
-    console.error(err);
+    console.log("The file was saved!");
   });
+})();
 
 function parsePage(browserPage, url) {
-  console.log(url);
-  // setTimeout(() => {
-    return browserPage.goto(url).then(function() {
-      return browserPage.content();
-    }).then(html => {
-      const $ = cheerio.load(html, {
-        decodeEntities: false
-      });
-      console.log('****************************************', $('#text-container').html());
-      text += '======================================================================='
-      text += $('#text-container').html();
-      return Promise.resolve();
-    })
-  // }, 2000)
-}
-
-function promiseSerial(tasks) {
-  return tasks.reduce((promiseChain, currentTask) => {
-    return promiseChain.then(chainResults =>
-      currentTask.then(currentResult =>
-        [ ...chainResults, currentResult ]
-      )
-    );
-  }, Promise.resolve([])).then(arrayOfResults => {
-      return arrayOfResults;
-  });
+  return browserPage.goto(url).then(function() {
+    return browserPage.content();
+  }).then(html => {
+    const $ = cheerio.load(html, {
+      decodeEntities: false
+    });
+    text += '=======================================================================';
+    text += $('#text-container').html();
+    return Promise.resolve();
+  })
 }
