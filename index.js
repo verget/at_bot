2e74977c1d2a0fe6f02d5fda910d302053944d8f
verget <verget@wilix.org>
2019-05-19 19:53:27 +0300

    grab text by chapters and fb2 layout

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-let url = 'https://author.today/reader/21513/153798';
+let firstPageUrl = 'https://author.today/reader/21513/152034';
+const urlArray = firstPageUrl.split('/');
 const fs = require('fs');
-let text = '';
+let globalText = `
+<?xml version="1.0" encoding="utf-8"?>
+<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+<description><title-info><book-title>Книга из бота</book-title></title-info></description>`;
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const urlArray = url.split('/');
-  let pageNumber = urlArray.slice(-1)[0] * 1;
-  for (let i = pageNumber; i < 153800; i++) {
-    console.log(i);
-    urlArray[urlArray.length - 1] = ++pageNumber;
-    url = urlArray.join('/');
-    await parsePage(page, url);
+  let chapters = [];
+  try {
+    chapters = await getChapters(page, firstPageUrl);
+  } catch (e) {
+    console.error('no chapters found');
+    return;
   }
-  fs.writeFile("./test.fb2", text, function(err) {
+  let url = '';
+  for (let i = 0; i < chapters.length; i++) {
+    urlArray[urlArray.length - 1] = chapters[i].id;
+    url = urlArray.join('/');
+    try {
+      await parsePage(page, url);
+    } catch(e) {
+      console.log(e);
+      break;
+    }
+  }
+  fs.writeFile("./test.fb2", globalText, function(err) {
     if (err) {
       return console.log(err);
     }
@@ -23,15 +37,31 @@ let text = '';
   });
 })();
 
+function renderPage(browserPage, url) {
+  return browserPage.goto(url).then( _ => browserPage.content())
+}
+
+function getChapters(browserPage, url) {
+  return renderPage(browserPage, url).then(html => {
+    let chaptersStr = html.match(/^.*chapters:\s*\[(.*)],$/gm)[0];
+    if (chaptersStr) {
+      chaptersStr = chaptersStr.substring(22, chaptersStr.length - 1);
+      let chaptersArray = JSON.parse(chaptersStr);
+      if (chaptersArray && chaptersArray.length) {
+        return Promise.resolve(chaptersArray);
+      }
+      return Promise.reject();
+    }
+  })
+}
+
 function parsePage(browserPage, url) {
-  return browserPage.goto(url).then(function() {
-    return browserPage.content();
-  }).then(html => {
+  return renderPage(browserPage, url).then(html => {
     const $ = cheerio.load(html, {
       decodeEntities: false
     });
-    text += '=======================================================================';
-    text += $('#text-container').html();
+    const text = $('#text-container').html();
+    globalText += text;
     return Promise.resolve();
   })
 }
